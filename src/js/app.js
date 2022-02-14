2bcf6f9a5f342de9e31292a67b4f3a2c0f21866a
Maksym <bilanych.max98009@gmail.com>
2022-02-14 14:52:10 +0200

    add services section

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,10 @@ flsFunctions.isWebp();
 'use strict';
 
 import menu from "./modules/menu/menu.js";
-// import detectMobile from './modules/detectMobile/detect-mobile.js';
-
 
 window.addEventListener('DOMContentLoaded', () => {
   menu('.btn-menu');
-  // detectMobile();
+
 });
 
 /*
