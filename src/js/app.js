fe0a1d74ba213ca406e27c355659eb74ce598bbb
Maksym <bilanych.max98009@gmail.com>
2022-02-22 16:56:28 +0200

    fix add shadow

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,15 @@ import { glide } from "./modules/glide-slider/glide-slider.js";
 window.addEventListener('DOMContentLoaded', () => {
   menu('.btn-menu');
   glide.mount();
+
+  window.addEventListener('scroll', () => {
+    let scroll = Math.round(document.documentElement.scrollTop);
+    if (scroll > 50) {
+      document.querySelector('.header').classList.add('header_shadow');
+    } else {
+      document.querySelector('.header').classList.remove('header_shadow');
+    }
+  });
 });
 
 /*
